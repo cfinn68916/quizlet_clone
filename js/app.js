35643ebeb14e29b35eac44ecf6970d927ac7c53a
colin finn <cfinn68916@gmail.com>
2021-12-20 16:30:13 -0600

    change

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ var phase=getlocal(ph=true);
 var tf_q= [];//new Map();
 var tf_ind=0;
 var tf_ans=[];
+var tf_c_set=[];
 
 
 
@@ -49,16 +50,45 @@ function test_a(set=get_set()){
     tf_q= [];
     tf_ind=0;
     tf_ans=[];
+    tf_c_set=set;
     function cq(v,i,a){
         if(Math.random()>0.5){
-            tf_q.push([v[0]+'|'+v[1],1])
+            tf_q.push([v[0]+'|'+v[1],1]);
+        }else{
+            tf_q.push([v[0]+'|'+a[Math.floor(Math.random() * a.length)][1],0]);
         }
     }
-    set.forEach(cq)
+    set.forEach(cq);
+    document.getElementById('tf_question').innerHTML=tf_q[0][0];
+}
+function process_tf_res(){
+    var wrong=[];
+    tf_ans.forEach(function(v,i,a){
+        if(v!=tf_q[i][1]){
+            wrong.push(tf_q[i][0].split('|'));
+            
+    }});
+    document.getElementById('tf_test').style.display = 'none';
     
+    var w2=wrong.filter(function(v){return v[0];});
+    var new_set=[];
+    tf_c_set.forEach(function(v,i,a){if(!w2.includes(v[0])){new_set.push(v)}});
+    console.log(tf_c_set[5])
+    flashcards(set=new_set);
+}
 
+
+function process_tf_ans(ans){
+    tf_ans.push(ans);
+    tf_ind+=1;
+    if(tf_ans.length==tf_q.length){
+        process_tf_res();
+        return;
+    }
+    document.getElementById('tf_question').innerHTML=tf_q[tf_ind][0];
     
 }
+
 
 
 function next(){
