b8def90c1394632fb9d575da47b6e3b8ef43dbd7
colin finn <cfinn68916@gmail.com>
2022-01-05 19:01:40 -0600

    temporary commit

--- js/write.js
+++ js/write.js
@@ -1,10 +1,19 @@
 var write_ans=[];
 var write_q=[];
+var write_val=[];
 var write_ind=0;
 function write_test(set=get_set(setnum)){
     document.getElementById('write_test').style.display = 'block';
     document.getElementById('write_q').innerHTML = set[0][0];
-    write_q=set;
+    write_q=set.forEach(function(v,i,a){v[0]});
+    write_ans=set.forEach(function(v,i,a){v[1]});
+    write_val=[];
+    write_ind=0;
+}
+
+function write_next(){
+    write_ind+=1;
+
 }
 
 function end_test(){
